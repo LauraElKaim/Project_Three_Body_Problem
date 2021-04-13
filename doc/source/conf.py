68e96f2edeb1c24f1b8d47fd05c9054a51d91245
unknown <laura.el-kaim@domaine.fr>
2021-04-13 22:28:00 +0200

    Change theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,6 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_bootstrap_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -47,9 +48,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'bootstrap'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = sphinx_bootstrap_theme.get_html_theme_path()
