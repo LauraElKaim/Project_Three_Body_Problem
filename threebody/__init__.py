bbd9144c526e710a3f7deb4337f95f2794e3c561
Mohamed Fattouhy <m.fattouhy@hotmail.com>
2021-04-14 19:00:04 +0200

    add new function

--- threebody/__init__.py
+++ threebody/__init__.py
@@ -1,4 +1,6 @@
-__version__="0.0.1"
+__version__="0.1.0"
 from .EDO.EDO_3 import distance
 from .EDO.EDO_3 import f
-from .EDO.EDO_3 import iter
+from .EDO.EDO_3 import trajectories
+from .EDO.EDO_3 import visualisation
+from .EDO.EDO_3 import Animation
