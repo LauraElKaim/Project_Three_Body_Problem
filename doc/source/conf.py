0f539c4dea6beb995e2239100b2795029df69abc
unknown <laura.el-kaim@domaine.fr>
2021-04-14 10:56:06 +0200

    rtd theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,7 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import sphinx_bootstrap_theme
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -32,6 +32,7 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -48,9 +49,13 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bootstrap'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_static_path = ['_static']
+
+html_theme_options = {
+    'style_nav_header_background': 'black'
+}
