7bd84f6dfafd0fe28c719c858b909ad6d4221f0e
unknown <amine.tzn34@gmail.com>
2021-04-17 14:44:25 +0200

    modif tests

--- Tests/tests.py
+++ Tests/tests.py
@@ -11,7 +11,44 @@ class test_edo3(unittest.TestCase):
         Y.shape = (n, )
         self.assertIsInstance(X,Y)
 
-    def 
+
+    def test_fonction_f(self):
+        r.shape = (n, )
+        t.shape = (1,1)
+        G=6.67e-11 
+        m1 = 5.972e+24
+        m2 = 6.4185e+23
+        m3 = 1.989e+30 
+        AU= 1.496e+11
+        a1 = 1.0*1.496e+11
+        a2 = 1.52*1.496e+11
+        x_i1 = -1.0*1.496e+11
+        y_i1 = 0 
+        z_i1 = 0
+        v_x1i = 0
+        v_y1i = 29779.301841746023
+        v_z1i = 0
+        x_i2 = 1.52*1.496e+11
+        y_i2 = 0
+        z_i2 = 0
+        v_x2i = 0
+        v_y2i = 24154.203325249873
+        v_z2i = 0 
+        x_i3 = 0
+        y_i3 = 0
+        z_i3 = 0
+        v_x3i = 0
+        v_y3i = 0
+        v_z3i = 0
+        self.assertIsInstance(r, t, G, m1, m2, m3, AU, a1, a2, 
+        x_i1, y_i1, z_i1, v_x1i, v_y1i, v_z1i, x_i2, y_i2, z_i2,
+        v_x2i, v_y2i, v_z2i, x_i3, y_i3, z_i3, v_x3i, v_y3i, v_z3i)
+
+
+    def trajectories(self):
+        t_upper = 3600*24*687
+        h = 100
+        sel.assertIsInstance(t_upper, h)
 
 #from EDO import EDO_3
 
