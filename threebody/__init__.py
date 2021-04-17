524499c9cc2c8f9bc729783106eae94062a95b6f
Mohamed Fattouhy <m.fattouhy@hotmail.com>
2021-04-17 16:32:27 +0200

    cosmit

--- threebody/__init__.py
+++ threebody/__init__.py
@@ -1,6 +1,1 @@
-__version__="0.1.0"
-from .EDO.EDO_3 import distance
-from .EDO.EDO_3 import f
-from .EDO.EDO_3 import trajectories
-from .EDO.EDO_3 import visualisation
-from .EDO.EDO_3 import Animation
+__version__="0.1.1"
