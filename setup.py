d4746971ff47ce38912248c09636f0aebec74e2d
Mohamed Fattouhy <m.fattouhy@hotmail.com>
2021-04-23 14:51:21 +0200

    Add Test module

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(
   name='threebody',
   version='0.1.1',
   description='A python package to visualize three body problem in 3D',
-  url='https://github.com/LauraElKaim/Project_Three_Body_Problem.git',
+  url='https://github.com/LauraElKaim/Project_Three_Body_Problem',
   author='Mohamed Fattouhy; Amine Touzani; Gueladio Niasse; Laura El Kaim',
   packages=find_packages(include=['threebody',
                                   'threebody.EDO',
-                                  'threebody.Vis'])
+                                  'threebody.Vis',
+                                  'threebody.Tests'])
 )
