69911be8361a33d427af2fb8553c1e8188549c83
unknown <laura.el-kaim@domaine.fr>
2021-04-24 21:47:11 +0200

    Update script

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 from threebody.EDO.EDO_3 import trajectories
-from threebody.Vis.visualisation import visualisation, Animation
+from threebody.Vis.visualisation import visualisation
+from threebody.Vis.visualisation import Animation
 from path import Path
 import moviepy.editor as mp
 
@@ -10,9 +11,9 @@ x_pnts1, y_pnts1, x_pnts2, y_pnts2, x_pnts3, y_pnts3, z_pnts1, z_pnts2, z_pnts3 
 
 # We call the visualisation function to create some images
 visualisation(nbr_images=200, save_path='threebody/Vis/Images/Images_2',
-                x1=x_pnts1, y1=y_pnts1, z1=z_pnts1,
-                x2=x_pnts2, y2=y_pnts2, z2=z_pnts2,
-                x3=x_pnts3, y3=y_pnts3, z3=z_pnts3)
+              x1=x_pnts1, y1=y_pnts1, z1=z_pnts1,
+              x2=x_pnts2, y2=y_pnts2, z2=z_pnts2,
+              x3=x_pnts3, y3=y_pnts3, z3=z_pnts3)
 
 
 # Create a gif from list of png file
@@ -23,4 +24,3 @@ visualisation(nbr_images=200, save_path='threebody/Vis/Images/Images_2',
 # Convert a gif created to mp4 file
 # clip = mp.VideoFileClip("threebody/Vis/Gifs/Three_body.gif")
 # clip.write_videofile("./myvideo.mp4")
-
