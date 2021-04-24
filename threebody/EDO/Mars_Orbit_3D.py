5fb48daa4710cd52ddc522c3f88a55afd513af13
unknown <laura.el-kaim@domaine.fr>
2021-04-24 21:21:45 +0200

    Update Mars_Orbit_3D

--- threebody/EDO/Mars_Orbit_3D.py
+++ threebody/EDO/Mars_Orbit_3D.py
@@ -3,11 +3,9 @@ import numpy as np
 import mpl_toolkits.mplot3d.axes3d as p3
 from matplotlib import animation
 from threebody.EDO.EDO_3 import trajectories
-from IPython.display import Video
+# from IPython.display import Video
 import moviepy.editor as mp
-from IPython.display import Video
-
-
+# from IPython.display import Video
 
 
 class Mars():
@@ -19,7 +17,6 @@ class Mars():
     when considering the Sun at the center of space.
 
     """
-
 
     def init(self):
 
@@ -33,37 +30,30 @@ class Mars():
         self.postion_y = 0
         self.postion_z = 0
 
-
     def str(self):
 
         return f"""                The Mars'mass: {self.mass} \n
-                The velocity'mass in x direction: {self.velocity_x} \n 
+                The velocity'mass in x direction: {self.velocity_x} \n
                 The velocity'mass in y direction: {self.velocity_y} \n
                 The velocity'mass in y direction: {self.velocity_y} \n
-                The velocity'mass in z in direction: {self.velocity_z} \n 
+                The velocity'mass in z in direction: {self.velocity_z} \n
                 The Mars'position in x direction: {self.postion_x} \n
                 The Mars'position in y direction: {self.postion_y} \n
                 The Mars'position in z direction: {self.postion_z}"""
 
 
-
-
 def Mars_Orbit(save_anim=False, fps=24, save_as_mp4=False):
 
-
     x_pnts1, y_pnts1, x_pnts2, y_pnts2, x_pnts3, y_pnts3, z_pnts1, z_pnts2, z_pnts3 = trajectories(h=100)
-
 
     # Create figure 3D
     fig = plt.figure()
     ax = p3.Axes3D(fig)
 
-
     # We create some random points to represent stars
     X = np.random.randint(-4e+11, 4e+11, 50, dtype=np.int64)
     Y = np.random.randint(-4e+11, 4e+11, 50, dtype=np.int64)
     Z = np.random.randint(-5000*3, 5000*3, 50, dtype=np.int64)
-
 
     # Plot some stars
     ax.scatter(X, Y, Z, s=0.05, marker='x', c='white')
@@ -71,18 +61,15 @@ def Mars_Orbit(save_anim=False, fps=24, save_as_mp4=False):
     ax.scatter(-1*X, -1*Y, -1*Z, s=0.05, marker='x', c='darkorange')
     ax.scatter(-1*X, -0.5*Y, Z, s=0.05, marker='x', c='purple')
 
-
     # We make the grid lines transparent
-    ax.xaxis._axinfo["grid"]['color'] =  (1, 1, 1, 0)
-    ax.yaxis._axinfo["grid"]['color'] =  (1, 1, 1, 0)
-    ax.zaxis._axinfo["grid"]['color'] =  (1, 1, 1, 0)
-
+    ax.xaxis._axinfo["grid"]['color'] = (1, 1, 1, 0)
+    ax.yaxis._axinfo["grid"]['color'] = (1, 1, 1, 0)
+    ax.zaxis._axinfo["grid"]['color'] = (1, 1, 1, 0)
 
     # set the parameters for the visual
     ax.set_axis_off()
     ax.set_facecolor('#070E25')
     ax.view_init(elev=32)
-
 
     def gen(n, x, y, z):
 
@@ -94,7 +81,6 @@ def Mars_Orbit(save_anim=False, fps=24, save_as_mp4=False):
             yield np.array([x[t], y[t], z[t]])
             t += n
 
-
     def update(num, data, line):
 
         """Set line"""
@@ -102,26 +88,20 @@ def Mars_Orbit(save_anim=False, fps=24, save_as_mp4=False):
         line.set_data(data[:2, :num])
         line.set_3d_properties(data[2, :num])
 
-
     N = 3000
     data = np.array(list(gen(n=N, x=x_pnts2, y=y_pnts2, z=z_pnts2))).T
     line, = ax.plot(data[0, 0:1], data[1, 0:1], data[2, 0:1],
-                        c='red', linestyle=":")
+                    c='red', linestyle=":")
 
     ax.plot3D(0, 0, 0, 'o', markersize=50, color='darkorange')
 
-
     ax.text(-4.5e+11, 0, 7500, 'Sun', c='white', fontsize=8,
-                        bbox=dict(facecolor='darkorange', edgecolor='white',
-                        boxstyle='circle,pad=0.5'))
-
-
+            bbox=dict(facecolor='darkorange', edgecolor='white',
+                      boxstyle='circle,pad=0.5'))
 
     ax.text(-3.8e+11, 0, 7900, 'Mars', c='white', fontsize=6,
-                    bbox=dict(facecolor='red', edgecolor='white',
-                    boxstyle='circle,pad=0.3'))
-
-     
+            bbox=dict(facecolor='red', edgecolor='white',
+                      boxstyle='circle,pad=0.3'))
 
     # Set x, y and z limits
     ax.set_xlim3d([-2.5e+11, 2.5e+11])
@@ -134,21 +114,21 @@ def Mars_Orbit(save_anim=False, fps=24, save_as_mp4=False):
     ax.set_zlabel('Z')
 
     animation_Mars = animation.FuncAnimation(fig, update,
-                                                frames=200, fargs=(data, line),
-                                                interval=10, blit=False)
+                                             frames=200, fargs=(data, line),
+                                             interval=10, blit=False)
 
     # save the gif if save_anim is True
     if save_anim:
 
         animation_Mars.save('Mars_orbit.gif', fps=fps, writer='ffmpeg')
 
-
     # save the gif as mp4 if save_as_mp4 is True
     if save_as_mp4:
 
         clip = mp.VideoFileClip("Mars_orbit.gif")
         clip.write_videofile("myvideo.mp4")
-    
+
     plt.show()
+
 
 Mars_Orbit(save_anim=True, fps=24)
