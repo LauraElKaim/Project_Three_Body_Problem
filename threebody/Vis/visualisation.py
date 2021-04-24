946841fb0717598c6e14de5b1c2606330478dee3
unknown <laura.el-kaim@domaine.fr>
2021-04-25 00:49:05 +0200

    Update visualisation

--- threebody/Vis/visualisation.py
+++ threebody/Vis/visualisation.py
@@ -8,13 +8,12 @@ import os.path
 import moviepy.editor as mp
 
 
-
 def visualisation(nbr_images, save_path, x1, y1, z1,
-   x2, y2, z2, x3, y3, z3):
-
+                  x2, y2, z2, x3, y3, z3):
 
     """
-    Create figures of the trajectories and orbits of each body and save them in Images directory
+    Create figures of the trajectories and orbits of each body \n
+    and save them in Images directory
 
     :param nbr_images: The number of image (png file) to make the visualisation
     :type nbr_image: integer
@@ -42,11 +41,12 @@ def visualisation(nbr_images, save_path, x1, y1, z1,
 
     :param y3: y coordinate of body 3
     :type y3: vector of shape (n,) or (n,1)
-    
+
     :param z3: z coordinate of body 3
     :type z3: vector of shape (n,) or (n,1)
 
-    :param save_path: path in which you want to save the images, default: '.' (current directory)
+    :param save_path: path in which you want to save the images, \n
+    default: '.' (current directory)
     :type save_path: str
     """
 
@@ -74,9 +74,7 @@ def visualisation(nbr_images, save_path, x1, y1, z1,
         ax.view_init(elev=18)
 
         ax.get_proj = lambda: np.dot(p3.Axes3D.get_proj(ax),
-                        np.diag([1.6, 1.6, 0.9, 1]))
-
-
+                                     np.diag([1.6, 1.6, 0.9, 1]))
 
         # We plot the stars in different colors
         ax.scatter(X, Y, Z, s=0.1, marker='x', c='white')
@@ -84,11 +82,9 @@ def visualisation(nbr_images, save_path, x1, y1, z1,
         ax.scatter(-1*X, -1*Y, -1*Z, s=0.1, marker='x', c='darkorange')
         ax.scatter(-1*X, -0.5*Y, Z, s=0.1, marker='x', c='purple')
 
-       
         ax.xaxis.set_pane_color((0.06, 0.06, 0.06, 0.99))
         ax.yaxis.set_pane_color((0.1, 0.1, 0.1, 0.99))
         ax.zaxis.set_pane_color((0.1, 0.1, 0.1, 0.99))
-
 
         # We plot a trajectories of each body
         ax.plot3D(x1, y1, z1, 'white', linewidth=1, alpha=1)
@@ -97,11 +93,10 @@ def visualisation(nbr_images, save_path, x1, y1, z1,
         ax.plot3D(x1, y1, z1, 'cornflowerblue', linewidth=0.9)
         ax.plot3D(x2, y2, z2, 'tomato', linewidth=0.9)
 
-
         # We make the grid lines transparent
-        ax.xaxis._axinfo["grid"]['color'] =  (1, 1, 1, 0)
-        ax.yaxis._axinfo["grid"]['color'] =  (1, 1, 1, 0)
-        ax.zaxis._axinfo["grid"]['color'] =  (1, 1, 1, 0)
+        ax.xaxis._axinfo["grid"]['color'] = (1, 1, 1, 0)
+        ax.yaxis._axinfo["grid"]['color'] = (1, 1, 1, 0)
+        ax.zaxis._axinfo["grid"]['color'] = (1, 1, 1, 0)
 
         # Set x, y and z limits
         ax.set_xlim3d([-2.5e+11, 2.5e+11])
@@ -113,43 +108,35 @@ def visualisation(nbr_images, save_path, x1, y1, z1,
         ax.set_zlim3d([-5000, 5000])
         ax.set_zlabel('Z')
 
-
         # We create a ball that represents the sun
         ax.plot3D(0, 0, 0, 'o', markersize=50, color='darkorange')
-
 
         # We plot the postion of each bodies at each time t
         ax.plot(x1[t], y1[t], z1[t], 'o',
                 markersize=7, color='darkblue')
-        ax.plot(x2[t], y2[t], z2[t], 'o', 
-                markersize=5, color='darkred')
-
+        ax.plot(x2[t], y2[t], z2[t], 'o', markersize=5, color='darkred')
 
         for t_month in l_month:
 
             if t_month*(len(x1)/23) <= t <= (t_month+1)*(len(x1)/23):
 
                 tm = 'Elapsed time : ' + str(l_month[t_month]) + ' Earth month'
-                ax.text(0.6e+11, 0, 11500, tm, c='black',  fontsize=8, va='top',
+                ax.text(0.6e+11, 0, 11500, tm, c='black', fontsize=8, va='top',
                         bbox=dict(facecolor='white', edgecolor='white',
-                        boxstyle='round,pad=0.6'))
-
+                                  boxstyle='round,pad=0.6'))
 
         # Add legend
         ax.text(-2.7e+11, 0, 9000, 'Sun', c='white', fontsize=8,
-                        bbox=dict(facecolor='darkorange', edgecolor='white',
-                        boxstyle='circle,pad=0.5'))
-
+                bbox=dict(facecolor='darkorange', edgecolor='white',
+                          boxstyle='circle,pad=0.5'))
 
         ax.text(-2.2e+11, 0, 9200, 'Earth', c='white', fontsize=6,
-                        bbox=dict(facecolor='darkblue', edgecolor='white',
-                        boxstyle='circle,pad=0.3'))
-
+                bbox=dict(facecolor='darkblue', edgecolor='white',
+                          boxstyle='circle,pad=0.3'))
 
         ax.text(-1.7e+11, 0, 9400, 'Mars', c='white', fontsize=6,
-                        bbox=dict(facecolor='darkred', edgecolor='white',
-                        boxstyle='circle,pad=0.3'))
-
+                bbox=dict(facecolor='darkred', edgecolor='white',
+                          boxstyle='circle,pad=0.3'))
 
         # We make some pause
         plt.pause(0.00000001)
@@ -162,7 +149,6 @@ def visualisation(nbr_images, save_path, x1, y1, z1,
 
         # We close the figure to make the next one
         plt.close()
-
 
 
 class Animation():
@@ -182,16 +168,14 @@ class Animation():
 
         print("Instance Created, you will create a gif.")
 
-
     def __call__(self, image_path, fps, gif_path):
-
 
         """This function create a gif from a list of png file
 
         :param image_path: Path of the list of png files to create the gif
         :type imag_path: str
 
-        :param fps: Frames per second for the gif 
+        :param fps: Frames per second for the gif
         :type fps: integer
 
         :param gif_path: The path where to put the gif created
@@ -207,7 +191,6 @@ class Animation():
         self.images = list(self.image_path.glob('*.png'))
         self.image_list = []
 
-
         # We test if the path where the png files are located exist or not
         # If not, we raise an error
         try:
@@ -216,15 +199,13 @@ class Animation():
         except Exception as error:
             print("Caught an exception:", error)
 
-
-        # We check if the list wich contain the png file 
+        # We check if the list wich contain the png file
         # is not empty
         try:
             assert len(self.images) != 0
 
         except Exception as error:
             print("Caught an exception:", error)
-
 
         # We test if the fps parameter is an integer or not
         # If not, we raise an error
@@ -233,7 +214,6 @@ class Animation():
 
         except Exception as error:
             print("Caught an exception:", error)
-
 
         # We make a loop to read the png file
         # and add them in image list
